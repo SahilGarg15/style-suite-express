678c4244323650e717a7641118f3a3916bac3530
Sahil <gargsahil156@gmail.com>
2025-11-02 02:10:22 +0530

    Move prisma client to api directory for serverless functions

--- api/orders/create.ts
+++ api/orders/create.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { prisma } from '../../lib/prisma'
+import { prisma } from '../prisma'
 import jwt from 'jsonwebtoken'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
